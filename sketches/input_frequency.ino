47e5e3970da853353d5825fd53ac1bd014013912
Praseetha-KR <praseetha04@gmail.com>
2017-07-23 12:24:40 +0530

    Changes constant case

--- sketches/input_frequency.ino
+++ sketches/input_frequency.ino
@@ -5,30 +5,30 @@ const int BAUDRATE = 9600;
 const char DELIMITER = '\n';
 const char READY_SYMBOL = 'R';
 
-const double defaultFreq = 91.1;
-const char dummyChar = '-';
-const int bufSize = 5;
+const double DEFAULT_FREQUENCY = 91.1;
+const char DUMMY_CHAR = '-';
+const int BUFFER_SIZE = 5;
 
 TEA5767 radio;
 double freq;
-char buf[bufSize];
+char buf[BUFFER_SIZE];
 
 void resetBuf() {
-  for (int i = 0; i < bufSize; i++) {
-    buf[i] = dummyChar;
+  for (int i = 0; i < BUFFER_SIZE; i++) {
+    buf[i] = DUMMY_CHAR;
   }
 }
 
 void setup() {
   Serial.begin(BAUDRATE);
   radio.init();
-  radio.set_frequency(defaultFreq);
+  radio.set_frequency(DEFAULT_FREQUENCY);
   resetBuf();
   Serial.write(READY_SYMBOL);
 }
 
 void loop() {
-  if (Serial.readBytesUntil(DELIMITER, buf, bufSize) > 0) {
+  if (Serial.readBytesUntil(DELIMITER, buf, BUFFER_SIZE) > 0) {
     freq = atof(buf);
     radio.set_frequency(freq);
     Serial.println(freq);
