897002ef38b4357f1ec542a636ab7c88f09e0c26
Praseetha-KR <praseetha04@gmail.com>
2017-07-23 10:01:09 +0530

    Pulls out constants

--- sketches/input_frequency.ino
+++ sketches/input_frequency.ino
@@ -1,36 +1,38 @@
 #include <Wire.h>
 #include <TEA5767.h>
 
+const int BAUDRATE = 9600;
+const char DELIMITER = '\n';
+const char READY_SYMBOL = 'R';
+
 const double defaultFreq = 91.1;
-const int dataRate = 9600;
 const char dummyChar = '-';
+const int bufSize = 5;
 
 TEA5767 radio;
 double freq;
-
-const int bSize = 5;
-char buf[bSize];
+char buf[bufSize];
 
 void resetBuf() {
-  for (int i = 0; i < bSize; i++) {
+  for (int i = 0; i < bufSize; i++) {
     buf[i] = dummyChar;
   }
 }
 
 void setup() {
-  Serial.begin(dataRate);
+  Serial.begin(BAUDRATE);
   radio.init();
   radio.set_frequency(defaultFreq);
   resetBuf();
-  Serial.write('R');
+  Serial.write(READY_SYMBOL);
 }
 
 void loop() {
-  if (Serial.readBytesUntil('\n', buf, bSize) > 0) {
+  if (Serial.readBytesUntil(DELIMITER, buf, bufSize) > 0) {
     freq = atof(buf);
     radio.set_frequency(freq);
     Serial.println(freq);
-    Serial.write('R');
+    Serial.write(READY_SYMBOL);
   }
   Serial.flush();
   resetBuf();
