2ed41341b065f35650f30e9598d3711f617e351b
Praseetha-KR <praseetha04@gmail.com>
2017-07-23 12:18:25 +0530

    Adds station list route, Adds config file

---
+++ python/config.py
@@ -0,0 +1,23 @@
+SERIALPORT = '/dev/cu.usbmodem1411'
+BAUDRATE = 9600
+
+READY_SYMBOL = 'R'
+DELIMITER = '\n'
+WR_BUF_TIME = 0.5
+
+STATIONS_LIST = {
+    'BLR': {
+        '91.1': 'Radio City (Kannada)',
+        '91.9': 'Indigo (Englist)',
+        '92.7': 'Big FM (Kannada)',
+        '93.5': 'Red FM (Hindi)',
+        '94.3': 'Radio One (English)',
+        '95': 'Radio Mirchi (Hindi)',
+        '98.3': 'Radio Mirchi (Kannada)',
+        '100.1': 'Amrutavarshini (Classical music)',
+        '101.3': 'Rainbow FM (Kannada, English & Hindi)',
+        '102.9': 'Vividh Bharati (Kannada & Hindi)',
+        '104': 'Fever (Hindi)',
+        '106.4': 'Gyanvani (by IGNOU)'
+    }
+}
--- python/radio.py
+++ python/radio.py
@@ -2,28 +2,43 @@ from flask import Flask
 from time import sleep
 import serial
 
-
-SERIALPORT = '/dev/cu.usbmodem1411'
-BAUDRATE = 9600
-READY_SYMBOL = 'R'
-DELIMITER = '\n'
-WR_BUF_TIME = 0.5
-
 app = Flask(__name__)
-ser = serial.Serial(SERIALPORT, BAUDRATE)
+app.config.from_pyfile('config.py')
+
+try:
+    ser = serial.Serial(app.config.SERIALPORT, app.config.BAUDRATE)
+except:
+    print('ERROR: could not open port {}'.format(app.config.SERIALPORT))
+
+
+@app.route('/stations/<loc>')
+def stations(loc):
+    try:
+        loc_stations = app.config.STATIONS_LIST[loc.upper()]
+        loc_list = ''
+        for k in loc_stations:
+            loc_list += '<li><a href="/tune/{0}">{0}</a>: {1}</li>'.format(
+                k, loc_stations[k]
+            )
+    except:
+        loc_list = 'No stations listed'
+
+    return '<h3>FM Stations List: {}</h3><ul>{}</ul>'.format(
+        loc.upper(),
+        loc_list
+    )
 
 
 @app.route('/tune/<freq>')
 def tune(freq):
     msg = ser.read()
-    if (msg.decode('utf-8') == READY_SYMBOL):
-        fin_b = (str(freq) + DELIMITER).encode()
+    if (msg.decode('utf-8') == app.config.READY_SYMBOL):
+        fin_b = (str(freq) + app.config.DELIMITER).encode()
         ser.write(fin_b)
-        sleep(WR_BUF_TIME)
+        sleep(app.config.WR_BUF_TIME)
         fout_b = ser.readline()
         fout = fout_b.decode('utf-8')
-        print("Set frequency to {}".format(fout))
-        return "Now playing {}".format(fout)
+        return 'Now playing {}'.format(fout)
 
     else:
         return 'Not ready'
